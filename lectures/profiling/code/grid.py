a9bc9eac11cf526dfe67a4e0e66dbe2845dcdd3d
Simon Funke <simon.funke@gmail.com>
2017-01-29 16:32:36 +0100

    Work on the profiling lecture

--- lectures/profiling/code/grid.py
+++ lectures/profiling/code/grid.py
@@ -4,11 +4,15 @@ class Grid2D(object):
     def __init__(self,
                  xmin=0, xmax=1, dx=0.5,
                  ymin=0, ymax=1, dy=0.5):
+	""" Initialises a new 2D grid object """
         
         self.xcoor = arange(xmin, xmax+dx, step=dx)
         self.ycoor = arange(ymin, ymax+dy, step=dy)
 
     def gridloop(self, f):
+	""" Evaluate a function to each vertex of the grid.
+	    Returns: An array of all function values
+	"""
         lx = size(self.xcoor)
         ly = size(self.ycoor)
         a = zeros((lx,ly))
@@ -20,11 +24,11 @@ class Grid2D(object):
                 a[i,j] = f(x, y)
         return a
 
-g = Grid2D(dx=0.001, dy=0.001)
-
-
 def myfunc(x, y):
     return sin(x*y) + y
+
+# Initialise grid
+g = Grid2D(dx=0.001, dy=0.001)
 
 print("Computing values...")
 a = g.gridloop(myfunc)
