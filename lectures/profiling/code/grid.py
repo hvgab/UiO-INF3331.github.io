8dda38c3dee8f901740ef7758c954f7d0d8e6aa1
Simon Funke <simon.funke@gmail.com>
2017-01-29 21:37:38 +0100

    Fix syntax errors

--- lectures/profiling/code/grid.py
+++ lectures/profiling/code/grid.py
@@ -4,15 +4,15 @@ class Grid2D(object):
     def __init__(self,
                  xmin=0, xmax=1, dx=0.5,
                  ymin=0, ymax=1, dy=0.5):
-	""" Initialises a new 2D grid object """
+        """ Initialises a new 2D grid object """
         
         self.xcoor = arange(xmin, xmax+dx, step=dx)
         self.ycoor = arange(ymin, ymax+dy, step=dy)
 
     def gridloop(self, f):
-	""" Evaluate a function to each vertex of the grid.
-	    Returns: An array of all function values
-	"""
+        """ Evaluate a function to each vertex of the grid.
+            Returns: An array of all function values
+        """
         lx = size(self.xcoor)
         ly = size(self.ycoor)
         a = zeros((lx,ly))
@@ -32,4 +32,4 @@ g = Grid2D(dx=0.001, dy=0.001)
 
 print("Computing values...")
 a = g.gridloop(myfunc)
-print("done")        
+print("done")
