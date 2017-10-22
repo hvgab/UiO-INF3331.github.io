df0f6b56662527d556c6c6d1a2bd37f69bb10236
hvgab <31376076+hvgab@users.noreply.github.com>
2017-10-23 01:49:14 +0200

    easy debug
    Easy debug of diff between expected and actual output with colorama and difflib.

--- assignments/teststuite/run_tests.py
+++ assignments/teststuite/run_tests.py
@@ -1,7 +1,28 @@
-#!/bin/python3
+
 import sys
 import difflib
 import imp
+
+# Colored diff lines - "pip install colorama" for colored output
+try:
+    from colorama import Fore, Back, Style, init
+    init()
+except ImportError:  # fallback so that the imported classes always exist
+    class ColorFallback():
+        __getattr__ = lambda self, name: ''
+    Fore = Back = Style = ColorFallback()
+
+
+def color_diff(diff):
+    for line in diff:
+        if line.startswith('+'):
+            yield Fore.GREEN + line + Fore.RESET
+        elif line.startswith('-'):
+            yield Fore.RED + line + Fore.RESET
+        elif line.startswith('^'):
+            yield Fore.BLUE + line + Fore.RESET
+        else:
+            yield line
 
 try:
     parser_fn, scraper_fn = sys.argv[1], sys.argv[2]
@@ -38,16 +59,24 @@ for test_input_fn, function in test_suite:
         expected_output = "".join(f.readlines())
 
     actual_output = function(test_input)
-    
-    if type(actual_output) is list: # convert lists of mails/hyperlinks to text
+
+    if type(actual_output) is list:  # convert lists of mails/hyperlinks to text
         actual_output = "\n".join(actual_output)
-        
+
     if expected_output != actual_output:
         print(
             "On {}, expected output was\n{}\n and actual output was\n{}".format(
                 test_input_fn, expected_output, actual_output
             )
         )
+
+        # DIFFLINES FOR EASY DEBUG
+        print(Back.WHITE + Fore.BLACK + '***** START DEBUG DIFFLINES *****' + Fore.RESET + Back.RESET)
+        diff = difflib.ndiff(expected_output.split('. '), actual_output.split('. '))
+        diff = color_diff(diff)
+        # print(''.join(diff))
+        print('\n'.join(diff), end="")
+        print(Back.WHITE + Fore.BLACK + '***** END DEBUG DIFFLINES *****' + Fore.RESET + Back.RESET)
     else:
         N_passed += 1
 
